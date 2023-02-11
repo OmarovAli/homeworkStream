ec3c8da6a002bb985adc959a6f3b86bfa1f607a4
Ali Omarov <omarovali425@gmail.com>
2023-02-11 21:42:40 +0300

    file changed

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        intList.stream()
-                .filter(x -> x > 0)
-                .filter(x -> x % 2 == 0)
-                .sorted(Comparator.naturalOrder())
-                .forEach(System.out::println);
+    List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        for (int x : intList) {
+        if (x > 0 && x % 2 == 0) {
+            System.out.println(x);
+            }
+        }
     }
 }
